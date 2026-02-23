337cda2c21fd55889e9f1fb9c4b2580ec1506746
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-02-23 22:19:49 +0700

    Update app_phase1.py

--- app_phase1.py
+++ app_phase1.py
@@ -384,10 +384,12 @@ def display_styled_table(df):
 
     # Terapkan styling
     styled_df = df.style.format(format_dict, na_rep="-")
-    
+
     if growth_cols:
         styled_df = styled_df.map(apply_growth_color, subset=growth_cols)
 
+    # 🔥 TAMBAHKAN INI (freeze kolom pertama)
+    styled_df = styled_df.set_sticky(axis="columns")
     # Tampilkan di Streamlit
     st.dataframe(
         styled_df,
