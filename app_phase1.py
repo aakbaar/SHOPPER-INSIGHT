30e13ff05ebf43120a23cf98a11b85ff098175d9
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-02-24 08:22:54 +0700

    Update app_phase1.py

--- app_phase1.py
+++ app_phase1.py
@@ -787,9 +787,15 @@ def main():
         SHOPPER<br>
         CATEGORY<br>
         INSIGHT
-        <div style="margin-top:8px; font-size:14px; font-weight:500; color:#94A3B8; letter-spacing:1px;">
-            PHASE 2
-        </div>
+        <br>
+        <span style="
+            font-size:14px;
+            font-weight:500;
+            color:#000000;
+            letter-spacing:1px;
+        ">
+            PHASE 1
+        </span>
     </p>
     """, unsafe_allow_html=True)
     
