29abf6acb6130d99474442fdda5fc538928772af
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-02-26 06:15:39 +0700

    Update app_phase1.py

--- app_phase1.py
+++ app_phase1.py
@@ -783,7 +783,7 @@ def render_switching_cards(total_sw, total_no, top_dest_name, top_dest_pct):
         background-color: #F8F9FA; 
         border-radius: 8px; 
         padding: 12px; 
-        height: 100px; 
+        height: 80px; 
         border: 1px solid #EEE;
         display: flex;
         flex-direction: column;
