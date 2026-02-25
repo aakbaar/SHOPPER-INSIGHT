cc8d1ee64b3b4646e3eedea6c371dfe347475b9d
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-02-26 05:39:24 +0700

    Update app_phase1.py

--- app_phase1.py
+++ app_phase1.py
@@ -503,8 +503,8 @@ def display_styled_table(df):
 
     # 6. Tampilkan ke Streamlit
     st.dataframe(
-        styled_df,
-        use_container_width=True, 
+        df,
+        use_container_width=True,
         hide_index=True
     )
     
