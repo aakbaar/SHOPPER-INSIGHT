37b1a31d2ad84eef0b9cf1114a19a7b14f7be9b2
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-03-10 04:57:02 +0700

    Update app_phase1.py

--- app_phase1.py
+++ app_phase1.py
@@ -1279,9 +1279,9 @@ def main():
         col_title, col_plano, col_sec = st.columns([5, 2.5, 2.5])
         with col_title:
             st.markdown("""
-            <h1 style="margin:0;font-weight:500;">
+            <h2 style="margin:0;font-weight:800;">
             📈 PERFORMANCE OVERVIEW
-            </h1>
+            </h2>
             """, unsafe_allow_html=True)
         with col_plano: sel_plano = st.selectbox(
                             "VERSI PLANO",
