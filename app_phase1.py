a0f14b8ceeca002071daf528a6bd45ec7dbff9f7
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-03-10 08:46:55 +0700

    Update app_phase1.py

--- app_phase1.py
+++ app_phase1.py
@@ -231,7 +231,7 @@ st.markdown("""
 /* Kecilkan lebar layout utama */
 .block-container {
     max-width: 1400px;
-    padding-top: 0.5rem;
+    padding-top: 1rem;
 }
 
 html, body, [class*="css"]  {
